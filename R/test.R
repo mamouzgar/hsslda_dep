36ca0840c0dc90d853c73f9cae583b8ddca8ac3a
Meelad Amouzgar <mamouzgar@DNa80e92e.SUNet>
2021-07-06 10:30:22 -0700

    commented test script out

--- R/test.R
+++ R/test.R
@@ -8,30 +8,30 @@
 # library(dplyr)
 # library(magrittr)
 # library(ggplot2)
-path  <- "~/phd-projects/sc-lda/data/analysis-ready/metabolism-CD8-naive-data_cell-allmarkers.csv"
-channels <- c('GLUT1', 'HK2', 'GAPDH', 'LDHA', 'MCT1', 'PFKFB4', 'IDH2', 'CyclinB1',
-              'GLUD12', 'CS', 'OGDH', 'CytC', 'ATP5A', 'S6_p', 'HIF1A', 'PDK1_p', 'NRF1',
-              'NRF2_p', 'XBP1', 'VDAC1', 'OPA1', 'DRP1', 'ASCT2', 'GLS', 'GOT2', 'CPT1A',
-              'ACADM', 'IdU', 'BrU', 'Puromycin', 'H3_p',
-              "CD45RA",
-              # "CD69","CD25"
-              "CD69","CD3","CD98","CD25","CD27","CD137","CD57"
-
-              # 'DNA','barium'
-)
-dat_input <- data.table::fread(path)
-dat <- dat_input %>%
-  filter(H3_p < 0.05,
-         dead < 0.3)  %>%
-  group_by(labels) %>%
-  sample_n(1000) %>%
-  data.frame() %>%
-  dplyr::select(all_of(channels[1:15]), labels)
+# path  <- "~/phd-projects/sc-lda/data/analysis-ready/metabolism-CD8-naive-data_cell-allmarkers.csv"
+# channels <- c('GLUT1', 'HK2', 'GAPDH', 'LDHA', 'MCT1', 'PFKFB4', 'IDH2', 'CyclinB1',
+#               'GLUD12', 'CS', 'OGDH', 'CytC', 'ATP5A', 'S6_p', 'HIF1A', 'PDK1_p', 'NRF1',
+#               'NRF2_p', 'XBP1', 'VDAC1', 'OPA1', 'DRP1', 'ASCT2', 'GLS', 'GOT2', 'CPT1A',
+#               'ACADM', 'IdU', 'BrU', 'Puromycin', 'H3_p',
+#               "CD45RA",
+#               # "CD69","CD25"
+#               "CD69","CD3","CD98","CD25","CD27","CD137","CD57"
 #
-# save(dat, file="data/TcellHartmann2020_sampleData.RData")
+#               # 'DNA','barium'
+# )
+# dat_input <- data.table::fread(path)
+# dat <- dat_input %>%
+#   filter(H3_p < 0.05,
+#          dead < 0.3)  %>%
+#   group_by(labels) %>%
+#   sample_n(1000) %>%
+#   data.frame() %>%
+#   dplyr::select(all_of(channels[1:15]), labels)
+# #
+# # save(dat, file="data/TcellHartmann2020_sampleData.RData")
 # write.table(dat, "data/TcellHartmann2020_sampleData.txt", sep="\t",row.names = FALSE, col.names = TRUE)
-save(dat, file="data/TcellHartmann2020_sampleData.rda")
-
+# save(dat, file="data/TcellHartmann2020_sampleData.rda")
+#
 #
 # # fwrite(dat, file = output_path)
 # # training data with appropriate channels
